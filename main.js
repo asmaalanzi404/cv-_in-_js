4296608357647d460540bdbb1fed45857ed6b177
asma <asmaalanzi404@gmail.com>
2023-08-21 01:36:13 +0300

    cv1

---
+++ main.js
@@ -0,0 +1,70 @@
+//JSX
+
+const react1 = document.getElementById("root");
+let element = <div>
+
+<div className="container">
+<h1>ASMA SHABAB ALENZI</h1>
+<h2>Front-End Developer</h2>
+
+<h3>CONTACT</h3>
+<div>Phone: 050311880</div>
+<div>Email: asmaalanzi404@gmail.com</div>
+<div>Location: Riyadh Region</div>
+<div>LinkedIn: <a href="https://linkedin.com/in/asma-alanzi">linkedin.com/in/asma-alanzi</a></div>
+
+<hr></hr>
+
+<h3>ABOUT ME</h3>
+    <p>
+      Obtaining a job opportunity in a supportive facility that allows upgrading the career ladder in accordance with the vision of the Kingdom of Saudi Arabia for 2030 by serving the community, contributing and raising the efficiency of the employer. I can also harness what I have learned to support the facility and raise productivity.
+    </p>
+
+    <hr></hr>
+
+    <h3>EDUCATION</h3>
+    <div>Bachelor of Computer Science</div>
+    <div> GPA: 4.60/5</div>
+    <div>Graduation date: 2022</div>
+    <div>Northern Border University</div>
+
+    <hr></hr>
+
+    <h3>EXPERIENCE</h3>
+      <div>JavaScript Bootcamp Trainee</div>
+      <div>Tuwaiq Academy - Riyadh</div>
+      <div>July 2023 - Present</div>
+
+      <hr></hr>
+
+      <h3>LANGUAGES</h3>
+    <ul>
+      <li>English</li>
+      <li>Arabic</li>
+    </ul>
+
+    <hr></hr>
+
+    <h3>SKILLS</h3>
+    <ul>
+      <li>Computer proficiency</li>
+      <li>Proficiency in dealing with others</li>
+      <li>The ability to withstand pressure</li>
+      <li>Intuition and quick comprehension</li>
+      <li>Adherence to working hours</li>
+    </ul>
+
+    <hr></hr>
+
+    <h3>TECHNICAL SKILLS</h3>
+    <ul>
+      <li>JavaScript</li>
+      <li>HTML</li>
+      <li>CSS</li>
+      <li>Bootstrap</li>
+      <li>Figma</li>
+    </ul>
+  </div>  
+</div>
+
+ReactDOM.render(element, react1);
